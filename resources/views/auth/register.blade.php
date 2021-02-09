d2177c7dc79d3ef178154f43e539581d8a099a4c
Marin Varnica <marinvarnica@gmail.com>
2021-02-09 18:41:43 +0100

    small fix on input type

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,7 +29,7 @@
                             <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username">
 
                                 @error('username')
                                 <span class="invalid-feedback" role="alert">
