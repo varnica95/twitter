14ada657fa311ee9f58153786a9acff5f2b6970e
marin <marinvarnica@gmail.com>
2021-02-17 19:02:49 +0100

    code cleanup

--- app/Http/Controllers/Api/Timeline/TimelineController.php
+++ app/Http/Controllers/Api/Timeline/TimelineController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Timeline;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Tweets\TweetCollection;
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 
 class TimelineController extends Controller
@@ -13,13 +14,13 @@ class TimelineController extends Controller
         $this->middleware('auth:sanctum');
     }
 
-    public function index(Request $request)
+    public function index(Request $request): TweetCollection
     {
         $tweets = $request->user()
             ->tweetsFromFollowing()
             ->latest()
             ->with(['user', 'likes'])
-            ->paginate(8);
+            ->paginate(5);
 
         return new TweetCollection($tweets);
     }
